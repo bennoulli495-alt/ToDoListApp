68063c3189a516b001d4e719d7c851f01f67866a
Bernoulli <bennoulli495@gmail.com>
2026-09-04 21:26:28 +0630

    Add core-splashscreen dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,5 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.2.0")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
